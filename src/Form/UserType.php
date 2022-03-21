ed36fe2bed9ebc7346cdafbcf7780055533d37e0
Kurunchi Chandra <ckurunchi@gmail.com>
2022-03-21 23:14:00 +0100

    add fields for user form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,11 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -21,8 +21,54 @@ class UserType extends AbstractType
         $builder
             ->add('email')
             ->add('username')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('country')
+            ->add('organization')
+            ->add('organizationPosition')
+            ->add('description', TextType::class)
+            ->add('email')
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices'  => [
+                    'Membre' => 'ROLE_TEAM_MEMBER',
+                    'Modérateur' => 'ROLE_TEAM_MANAGER',
+                    'Créateur' => 'ROLE_TEAM_CREATOR',
+                ],
+            ]);
+            
+            $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
+            
 //            ->add('isVerified')
-//            ->add('roles')
 //            ->add('teams')
         ;
         //hide password on the edit
