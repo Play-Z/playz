00414b15f3394201cea93487baec8db27aee6d56
Kurunchi Chandra <ckurunchi@gmail.com>
2022-03-21 22:10:17 +0100

    password security user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -28,7 +29,11 @@ class UserType extends AbstractType
             ->add('organizationPosition')
             ->add('description', TextType::class)
             ->add('email')
-            ->add('plainPassword', PasswordType::class, [
+            ;
+        //hide password on the edit
+        if($hidePsw !== "update"){
+
+            $builder->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
@@ -71,27 +76,6 @@ class UserType extends AbstractType
 //            ->add('isVerified')
 //            ->add('teams')
         ;
-        //hide password on the edit
-        if($hidePsw !== "update"){
-
-            $builder->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-                'label' => 'Password',
-            ]);
         }
     }
 
