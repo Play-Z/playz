0bda8568d6a9def096b082867e225ed5f70dcc84
Kurunchi Chandra <ckurunchi@gmail.com>
2022-03-29 22:51:10 +0200

    [WIP] feature reset user password from admin

--- src/Controller/admin/users/UsersController.php
+++ src/Controller/admin/users/UsersController.php
@@ -5,15 +5,31 @@ namespace App\Controller\admin\users;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use App\Form\AdminUserResetPasswordFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use SymfonyCasts\Bundle\ResetPassword\Controller\ResetPasswordControllerTrait;
+use SymfonyCasts\Bundle\ResetPassword\Exception\ResetPasswordExceptionInterface;
+use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 #[Route('/users')]
 class UsersController extends AbstractController
 {
+    use ResetPasswordControllerTrait;
+
+    private $resetPasswordHelper;
+
+    public function __construct(ResetPasswordHelperInterface $resetPasswordHelper)
+    {
+        $this->resetPasswordHelper = $resetPasswordHelper;
+    }
+    
     #[Route('/', name: 'users_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
@@ -91,4 +107,39 @@ class UsersController extends AbstractController
 
         return $this->redirectToRoute('users_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    #[Route('/{id}/edit/reset-user-password', name: 'request-reset-user-password', methods: ['GET'])]
+    public function request(Request $request, MailerInterface $mailer, User $user): Response
+    {
+        $form = $this->createForm(AdminUserResetPasswordFormType::class);
+        $form->handleRequest($request);
+        $user->getEmail();
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->processSendingPasswordResetEmail(
+                $form->get('email')->getData(),
+                $mailer
+            );
+        }
+
+        return $this->render('admin/users/password-reset/request-reset-user-password.html.twig', [
+            'requestForm' => $form->createView(),
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit/reset-user-password/check-email', name: 'user_reset_password_check_email')]
+    public function checkEmail(): Response
+    {
+        // Generate a fake token if the user does not exist or someone hit this page directly.
+        // This prevents exposing whether or not a user was found with the given email address or not
+        if (null === ($resetToken = $this->getTokenObjectFromSession())) {
+            $resetToken = $this->resetPasswordHelper->generateFakeResetToken();
+        }
+
+        return $this->render('reset_password/password-reset/check_email.html.twig', [
+            'resetToken' => $resetToken,
+        ]);
+    }
+    
+    
 }
